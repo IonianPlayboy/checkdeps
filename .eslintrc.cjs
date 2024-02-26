0076ad0937d114dfa20f9f0b319017e96ccccb8b
Nathanaël Labreuil <IonianPlayboy@users.noreply.github.com>
2024-02-26 22:24:09 +0100

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	root: true,
+	extends: ["@nuxt/eslint-config"],
+	rules: {
+		// Vue
+		"vue/multi-word-component-names": 0,
+		"vue/max-attributes-per-line": "off",
+		"vue/no-v-html": 0,
+		"vue/html-indent": 0,
+	},
+};
